bd9ba7cdf314160229b213e5eb98c0412d64f66f
hhoppework <hhoppe@ckr.com>
2025-03-18 17:28:35 -0500

    Add script to enable PrinterLogic Extension in Chrome and Edge

---
+++ Enable-PrinterLogicExtension.ps1
@@ -0,0 +1,20 @@
+$chromePath = 'C:\Program Files\Google\Chrome\Application\chrome.exe'
+if (Test-Path $chromePath) {
+    Write-Host "Google Chrome is installed. Enabling PrinterLogic Extension in Chrome."
+    try {
+        REG ADD "HKLM\SOFTWARE\Policies\Google\Chrome\ExtensionInstallForcelist" /v "1" /t REG_SZ /d "bfgjjammlemhdcocpejaompfoojnjjfn;https://clients2.google.com/service/update2/crx" /f -ErrorAction Stop
+        Write-Host "Chrome registry update succeeded."
+    } catch {
+        Write-Error "Error updating Chrome registry: $_"
+    }
+} else {
+    Write-Host "Google Chrome is not installed, cannot enable PrinterLogic Extension in Chrome."
+    return
+}
+Write-Host "Enabling PrinterLogic Extension in Microsoft Edge."
+try {
+    REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallForcelist" /v "1" /t REG_SZ /d "cpbdlogdokiacaifpokijfinplmdiapa;https://edge.microsoft.com/extensionwebstorebase/v1/crx" /f -ErrorAction Stop
+    Write-Host "Edge registry update succeeded."
+} catch {
+    Write-Error "Error updating Edge registry: $_"
+}
